3b1e05fab526ee8d7d6b427d6a16a084e280a995
Chris Anatalio <anataliocs@gmail.com>
2015-11-24 08:24:30 -0500

    Pull products by term API call data into carousel with angular

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,9 +1,30 @@
 'use strict';
 
 angular.module('canataliozapposApp')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($scope, $http, Principal) {
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
+
+        $scope.productSearchResults = {};
+        $scope.productSearchResultsList = [];
+
+        $scope.getProductsBySearchTerm = function() {
+            $http({
+                method: 'GET',
+                url: 'http://127.0.0.1:8080/api/products',
+                params: '',
+                headers: {}
+            }).success(function(data){
+                console.log(data);
+                $scope.productSearchResultsList = data.results;
+
+                console.log("results " + data.results);
+                console.log("productSearchResultsList " + $scope.productSearchResultsList[1].productId);
+            }).error(function(){
+                console.log("Error get products by term.");
+            });
+        };
+
         });
     });
